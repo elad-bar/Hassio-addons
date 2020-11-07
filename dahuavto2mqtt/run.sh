df2cd4821ab7230b92b7e1e69cbbacdd17675ccf
Elad Bar <eladba@payoneer.com>
2020-11-07 12:50:19 +0200

    Fix #1 #6 according to suggestion by dmitrychepel & jbleuzen

--- dahuavto2mqtt/run.sh
+++ dahuavto2mqtt/run.sh
@@ -9,6 +9,10 @@ export MQTT_BROKER_USERNAME=$(bashio::config 'mqtt.username')
 export MQTT_BROKER_PASSWORD=$(bashio::config 'mqtt.password')
 export MQTT_BROKER_TOPIC_PREFIX=$(bashio::config 'mqtt.topic_prefix')
 
+bashio::log.info "@@@@@@@@@@@@@@@@"
+which curl
+bashio::log.info "@@@@@@@@@@@@@@@@"
+bashio::log.info "FFS"
 bashio::log.info "Staring Dahua to MQTT"
 bashio::log.debug "Connecting to Intercom ${DAHUA_VTO_HOST} with username ${DAHUA_VTO_USERNAME}"
 bashio::log.debug "Connecting to Broker ${MQTT_BROKER_HOST} with username ${MQTT_BROKER_USERNAME}, Topic prefix: ${MQTT_BROKER_TOPIC_PREFIX}"
